741805f8917cf31724e8ffcc028a4d17f6571827
Jean Paul | Origem <jeanpaul2022@gmail.com>
2019-10-22 11:37:05 -0300

    Transformando Gesture em um componente

--- screens/JeanScreen.js
+++ screens/JeanScreen.js
@@ -1,57 +1,21 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { Component } from 'react';
 import {PanResponder, Animated, Easing, Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, CheckBox, Switch, TextInput, Button, Alert, Vibration, } from 'react-native';
+import GestureComp from '../components/Gesture/Gesture.component';
 
 export default class JeanScreen extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      pan: new Animated.ValueXY(),
-      scale: new Animated.Value(1),
-    };
-  }
 
-  componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
-      onPanResponderGrant: (evt, gestureState) => {
-        this.state.pan.setOffset({
-          x: this.state.pan.x._value,
-          y: this.state.pan.y._value
-        });
-        this.state.pan.setValue({x: 0, y: 0})
-        Animated.spring(this.state.scale,{
-          toValue: 1.3,
-          friction: 3
-        }).start();
-      },
-      onPanResponderMove: Animated.event([
-        null,
-        {dx: this.state.pan.x, dy: this.state.pan.y}
-      ]),
-      onPanResponderRelease: (evt, gestureState) => {
-        this.state.pan.flattenOffset();
-        Animated.spring(this.state.scale,{
-          toValue: 1,
-          friction: 3,
-        }).start();
-      }
-    });
+    };
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.animationView,
-        {transform: [
-          {translateX: this.state.pan.x},
-          {translateY: this.state.pan.y},
-          {scale: this.state.scale},
-          ]}]} {...this._panResponder.panHandlers}>
-          <Image></Image>
-        </Animated.View>
+        <GestureComp></GestureComp>
       </View>
     );
   }
@@ -66,12 +30,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  animationView: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'skyblue',
-    borderRadius: 50,
-    position: 'absolute'
   },
 })
