018b9a760aacc03cfae9d846b07b3900283b695a
Jean Paul | Origem <jeanpaul2022@gmail.com>
2019-10-22 11:38:06 -0300

    Transformando Gesture em um componente

---
+++ components/Gesture/Gesture.component.js
@@ -0,0 +1,66 @@
+import * as WebBrowser from 'expo-web-browser';
+import React, { Component } from 'react';
+import {PanResponder, Animated, Easing, Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, CheckBox, Switch, TextInput, Button, Alert, Vibration, } from 'react-native';
+
+export default class GestureComp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pan: new Animated.ValueXY(),
+      scale: new Animated.Value(1),
+    };
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        this.state.pan.setOffset({
+          x: this.state.pan.x._value,
+          y: this.state.pan.y._value
+        });
+        this.state.pan.setValue({x: 0, y: 0})
+        Animated.spring(this.state.scale,{
+          toValue: 1.3,
+          friction: 3
+        }).start();
+      },
+      onPanResponderMove: Animated.event([
+        null,
+        {dx: this.state.pan.x, dy: this.state.pan.y}
+      ]),
+      onPanResponderRelease: (evt, gestureState) => {
+        this.state.pan.flattenOffset();
+        Animated.spring(this.state.scale,{
+          toValue: 1,
+          friction: 3,
+        }).start();
+      }
+    });
+  }
+
+  render() {
+    return (
+        <Animated.View style={[styles.animationView,
+        {transform: [
+          {translateX: this.state.pan.x},
+          {translateY: this.state.pan.y},
+          {scale: this.state.scale},
+          ]}]} {...this._panResponder.panHandlers}>
+          <Image></Image>
+        </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  animationView: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'skyblue',
+    borderRadius: 50,
+    position: 'absolute'
+  },
+})
