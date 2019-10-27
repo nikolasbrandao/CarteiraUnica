265ba746c27009d54592d2362b84cab1c2c8ff5b
Jean Paul | Origem <jeanpaul2022@gmail.com>
2019-10-27 04:46:57 -0300

    Adicionando relogio

--- screens/JeanScreen.js
+++ screens/JeanScreen.js
@@ -17,14 +17,15 @@ export default class JeanScreen extends Component {
         {tipo: require('../assets/png/003-cloud.png'), temperatura_maxima: 35, temperatura_minima: 29, dia_semana: 'Domingo'},
         {tipo: require('../assets/png/049-rain.png'), temperatura_maxima: 27, temperatura_minima: 25, dia_semana: 'Segunda'},
         {tipo: require('../assets/png/004-storm.png'), temperatura_maxima: 22, temperatura_minima: 18, dia_semana: 'Terça'},
-        {tipo: require('../assets/png/015-snow.png'), temperatura_maxima: 33, temperatura_minima: 27, dia_semana: 'Quarta'},
-        {tipo: require('../assets/png/042-rain.png'), temperatura_maxima: 4, temperatura_minima: 1, dia_semana: 'Quinta'},
+        {tipo: require('../assets/png/042-rain.png'), temperatura_maxima: 33, temperatura_minima: 27, dia_semana: 'Quarta'},
+        {tipo: require('../assets/png/015-snow.png'), temperatura_maxima: 4, temperatura_minima: 1, dia_semana: 'Quinta'},
         {tipo: require('../assets/png/025-cloudy.png'), temperatura_maxima: 27, temperatura_minima: 22, dia_semana: 'Sexta'},
         {tipo: require('../assets/png/022-sun.png'), temperatura_maxima: 40, temperatura_minima: 36, dia_semana: 'Sábado'},
-      ]
+      ],
+      hora: '',
     };
   }
-
+  
 
   renderizarItem (item, index) {
     return(
@@ -38,13 +39,24 @@ export default class JeanScreen extends Component {
         )
   }
 
+  componentDidMount(){
+    const that = this;
+
+    const hour = new Date().getHours();
+    const min = new Date().getMinutes();
+
+    that.setState({
+      hora: hour + ':' + min,
+    });
+  }
+
   render() {
     return (
       <View style={{justifyContent: 'center', alignSelf: 'center', backgroundColor: '#D1F4F6',}}>
         <SafeAreaView/>
         <View style={styles.header}>
-          <Text style={styles.texto_H1}>Maranhão</Text>
-          <Text style={styles.texto_H2}>São Luís</Text>
+          <Text style={styles.texto_H1}>São Luis - MA</Text>
+          <Text style={styles.texto_H2}>{this.state.hora}</Text>
         </View>
         <FlatList
           horizontal = {true}
